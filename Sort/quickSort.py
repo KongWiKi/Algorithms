2df674366d286c0504cdd5837b93d9a387511a00
KongWiki <kongwiki@163.com>
2019-08-27 10:59:33 +0800

    add quick sort

--- Sort/quickSort.py
+++ Sort/quickSort.py
@@ -4,3 +4,22 @@
 @File: quickSort.py
 @Email: kongwiki@163.com
 """
+from Sort.extentions import randomList
+
+
+def quickSort(arrylist):
+	if not arrylist:
+		return []
+	pivot = arrylist[0]
+	preArray = sorted(filter(lambda x: x <= pivot, arrylist[1:]))
+	postArray = sorted(filter(lambda x: x >= pivot, arrylist[1:]))
+	return preArray + [pivot] + postArray
+
+
+if __name__ == '__main__':
+	print("==========     随机生成整数数组     ==========")
+	arraylist = randomList()
+	print(arraylist)
+	arraylist = quickSort(arraylist)
+	print("==========随机生成整数数组排序后结果==========")
+	print(arraylist)
